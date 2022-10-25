7799dc9e3606ef8ae71e186f52588ff0a73d3e46
AyrielNoriega <anoriega2310@gmail.com>
2022-10-25 10:17:06 -0500

    uso de fragment

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export default function FirstApp() {
+    return (
+    <>
+        <div>FirstApp</div>
+        <p>Parrafo</p>
+    </>
+    )
+}
---
+++ 03-counter-app-vite/src/HelloWordApp.jsx
@@ -0,0 +1,14 @@
+// function App() {
+//     return <h1>HelloWord app1</h1>
+// }
+
+// export default App;
+import FirstApp from './FirstApp';
+
+export const HelloWordApp = () => {
+  return (
+    <h2>HelloWord:
+        <FirstApp />
+    </h2>
+  )
+}
