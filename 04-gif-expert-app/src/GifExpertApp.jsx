83ee3e6ec783422ab3d89782cd24a2fa240bf0b3
AyrielNoriega <anoriega2310@gmail.com>
2022-11-06 16:24:46 -0500

    creando lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One punch', 'Gragon ball']);
   return (
     <>
         <h1>GifExpertApp</h1>
+        <ol>
+            { categories.map( category => {
+                return <li key={category}>{ category }</li>
+            }) }
+        </ol>
     </>
   )
 }
