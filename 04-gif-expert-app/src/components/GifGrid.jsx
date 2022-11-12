be92c5a95e4a637c2ed9df38c989eca517eb3025
AyrielNoriega <anoriega2310@gmail.com>
2022-11-12 00:18:03 -0500

    Mostrar los títulos de las imágenes

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,17 +1,32 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
 
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
 
-    getGifs();
+    useEffect( () => {
+        getImages();
+    }, []);
+
 
     return (
         <>
-            <div key={category}>
-                <h3>{category}</h3>
-                <p>{ category }</p>
-            </div>
+            <h5>{ category}</h5>
+            <ol>
+                {
+                    images.map( ({id, title}) => {
+                        return <li key={ id }>
+                                    { title }
+                                </li>
+                    })
+                }
+            </ol>
         </>
     )
 }
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
-export const getGifs = async() => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=0sF7hLaDGQ5Wq917TWPHHOjYAdcNhyA9&q=${ category}&limit=20`;
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=0sF7hLaDGQ5Wq917TWPHHOjYAdcNhyA9&q=${ category}&limit=10`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
