7420d0fe827894f80a8311054a8af4fd53c21b1f
AyrielNoriega <anoriega2310@gmail.com>
2022-11-19 16:21:23 -0500

    119. useEffect - SimpleForm

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'ayn@gmailc.com'
+    })
+
+    const { username, email } = formState;
+
+    const onInputChange = ({target}) => {
+        const { value, name } = target;
+        setFormState({
+            ...formState,
+            [ name ] : value
+        });
+    }
+
+    useEffect(() => {
+        console.log('effect');
+    });
+    
+
+    return (
+        <>
+            <h1>SimpleForm</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange={ onInputChange }
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="Username@gmail.com"
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+            />
+        </>
+    )
+}
