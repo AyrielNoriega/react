a494a269016c81a7815362e08362fcdb35f0f15a
AyrielNoriega <anoriega2310@gmail.com>
2022-11-30 00:14:36 -0500

    126. useFetch + useCounter

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './03-example/MultipleCustomHooks'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -9,7 +10,7 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FormWithCustomHook />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <MultipleCustomHooks />
+  // </React.StrictMode>
 )
