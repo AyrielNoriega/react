2fd7f4e82607e3c9d65b182d5940b46da2710638
AyrielNoriega <anoriega2310@gmail.com>
2022-12-18 23:55:40 -0500

    inicio de proyecto

---
+++ 07-heroes-spa/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { HeroesApp } from './HeroesApp';
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>,
+)
