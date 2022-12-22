6ebd54a59565c3b65b09c134c8b9a3812ff61552
AyrielNoriega <anoriega2310@gmail.com>
2022-12-22 11:19:32 -0500

    config router

--- 07-heroes-spa/src/main.jsx
+++ 07-heroes-spa/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import './styles.css'
-import { router } from './router';
+import { router } from './router/AppRouter';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import { HeroesApp } from '../HeroesApp';
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HeroesApp />,
+    },
+]);
