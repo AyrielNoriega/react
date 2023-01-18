ce3f8999648d7b5b917ffcf1cf3a91609cca7d7b
AyrielNoriega <anoriega2310@gmail.com>
2023-01-18 13:08:11 -0500

    244. AuthLayout

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,69 +1,53 @@
 import { Link as LinkRouter } from "react-router-dom";
 import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const LoginPage = () => {
 return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-        >
-            <Grid item
-                className="box-shadow"
-                xs={3}
-                sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
+        <AuthLayout title="Login">
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="correo"
+                            type="email"
+                            placeholder="correo@correo.com"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Contraseña"
+                            type="password"
+                            placeholder="Contraseña"
+                            fullWidth
+                        />
+                    </Grid>
 
-                <Typography variant="h5" sx={{ mb:1 }}>Login</Typography>
-
-                <form>
-                    <Grid conteiner>
-                        <Grid item xs={12} sx={{ mt: 2 }}>
-                            <TextField
-                                label="correo"
-                                type="email"
-                                placeholder="correo@correo.com"
-                                fullWidth
-                            />
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12} md={6}>
+                            <Button variant="contained" fullWidth>
+                                Login
+                            </Button>
                         </Grid>
-                        <Grid item xs={12} sx={{ mt: 2 }}>
-                            <TextField
-                                label="Contraseña"
-                                type="password"
-                                placeholder="Contraseña"
-                                fullWidth
-                            />
-                        </Grid>
-
-                        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-                            <Grid item xs={12} md={6}>
-                                <Button variant="contained" fullWidth>
-                                    Login
-                                </Button>
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <Button variant="contained" fullWidth>
-                                    <Google />
-                                    <Typography sx={{ ml: 1 }}>Google</Typography>
-                                </Button>
-                            </Grid>
-
-                        </Grid>
-
-                        <Grid container direction="row" justifyContent="end">
-                            <Link component={LinkRouter} to="/auth/register">
-                                Crear una cuenta
-                            </Link>
+                        <Grid item xs={12} md={6}>
+                            <Button variant="contained" fullWidth>
+                                <Google />
+                                <Typography sx={{ ml: 1 }}>Google</Typography>
+                            </Button>
                         </Grid>
 
                     </Grid>
-                </form>
 
-            </Grid>
+                    <Grid container direction="row" justifyContent="end">
+                        <Link component={LinkRouter} to="/auth/register">
+                            Crear una cuenta
+                        </Link>
+                    </Grid>
 
-        </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
     )
 }
