ea853c266bbc1117ad305727918533c270ac5345
AyrielNoriega <anoriega2310@gmail.com>
2023-07-23 15:14:36 -0500

    263. pokemonSlice

--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import { store } from './store';
+import { PokemonApp } from './PokemonApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            {/* <App /> */}
+            <PokemonApp />
         </Provider>
     </React.StrictMode>,
 )
