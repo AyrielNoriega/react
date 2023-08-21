10a8cabf0821412b3300d6fa1d34b8d7b86a432f
AyrielNoriega <anoriega2310@gmail.com>
2023-08-21 13:35:33 -0500

    paginar pokemons

--- 09-toolkit-redux/src/PokemonApp.jsx
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
 
     const dispatch = useDispatch();
+    const {isLoading, pokemons = [], page} = useSelector( state => state.pokemons)
 
     useEffect(() => {
         dispatch(getPokemons());
@@ -15,13 +16,19 @@ export const PokemonApp = () => {
         <>
             <h1>PokemonApp</h1>
             <hr />
-
+            <span>Loading: {isLoading ? 'True': 'False'}</span>
             <ul>
-                <li>hola</li>
-                <li>hola</li>
-                <li>hola</li>
-                <li>hola</li>
+                {pokemons.map((poke, index) => (
+                    <li key={index}>
+                        <span>{poke.name} </span>
+                        <a href={poke.url}>url</a>
+                    </li>
+                ))}
             </ul>
+            <button
+                disabled={isLoading}
+                onClick={ () => dispatch(getPokemons(page))}
+            >next</button>
         </>
     )
 }
